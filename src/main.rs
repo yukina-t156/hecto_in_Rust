ad70c5ec94835ed0ef231e738f017907a4722998
Yukina Tatsuta <ivy1200t@gmail.com>
2023-06-24 19:45:50 +0900

    chapter2

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 
-fn to_ctrl_byte(c: char) -> u8 {
-	let byte = c as u8;
-	// ctrl characters have the upper 3 bits set to 0.
-	// so, if byte is ctrl character, byte & 0b0001_1111 is same as byte.
-	byte & 0b0001_1111
+fn die(e: std::io::Error) {
+	panic!("{}",e);
 }
 
 fn main() {
 	let _stdout = stdout().into_raw_mode().unwrap();
 	
-    // キーボードの入力をbにバインド
-    for b in io::stdin().bytes() {
-	    // １文字ずつ出力
-		let b = b.unwrap();
-		let c = b as char;
-		if c.is_control() {
-			println!("{:?} \r", b);
-		} else {
-			println!("{:?} ({:?})\r", b, c);
+    // キーボードの入力をkeyにバインド
+    for key in io::stdin().keys() {
+		match key {
+			Ok(key) => match key {
+				Key::Char(c) => {
+					if c.is_control() {
+						println!("{:?} \r", c as u8);
+					} else {
+						println!("{:?} ({})\r", c as u8, c);
+					}
+				}
+				// cltr-Q can't be used in VSCode
+				Key::Ctrl('c') => break,
+				_ => println!("{:?}\r",key),
+			},
+			Err(err) => die(err),
 		}
-		// cltr-Q can't be used in VSCode
-	    if b == to_ctrl_byte('c') {
-		    break;
-	    }
 	}
 }
