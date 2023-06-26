8c7757aa13ab8963e4e0c062f70ccd159abce8db
Yukina Tatsuta <ivy1200t@gmail.com>
2023-06-26 10:40:31 +0900

    chap3 separating

--- src/editor.rs
+++ src/editor.rs
@@ -8,30 +8,35 @@ pub struct Editor {}
 impl Editor {
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
-	
-        // キーボードの入力をkeyにバインド
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    // cltr-Q can't be used in VSCode
-                    Key::Ctrl('c') => break,
-                    _ => println!("{:?}\r",key),
-                },
-                Err(err) => die(err),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
         }
     }
+    // 引数に&selfがない->static
     pub fn default() -> Self {
-        Editor{}
+        Self {}
+    }
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('c') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
     }
 }
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+
+
 
 fn die(e: std::io::Error) {
 	panic!("{}",e);
