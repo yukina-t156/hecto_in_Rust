9241818ed454770935d66e120a43e22967fd93f9
Yukina Tatsuta <ivy1200t@gmail.com>
2023-06-27 15:54:10 +0900

    Chapter3 -Clear the screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,14 +1,23 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
+
         loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
             if let Err(error) = self.process_keypress() {
                 die(error);
             }
@@ -16,12 +25,17 @@ impl Editor {
     }
     // 引数に&selfがない->static
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("{}", termion::clear::All);
+        io::stdout().flush()
+    }
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = read_key()?;
         match pressed_key {
-            Key::Ctrl('c') => panic!("Program end"),
+            Key::Ctrl('c') => self.should_quit = true,
             _ => (),
         }
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 mod editor;
 
 use editor::Editor;
 
 fn main() {
-	let editor = Editor::default();
-	editor.run();
+	Editor::default().run();
 }
