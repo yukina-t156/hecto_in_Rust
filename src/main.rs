89c30355c5de20e060583376d35ead354b0e594b
Yukina Tatsuta <ivy1200t@gmail.com>
2023-06-29 13:14:05 +0900

    Chapter3 -Window size

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod editor;
-
+mod terminal;
 use editor::Editor;
+pub use terminal::Terminal; // re-exporting the `Terminal` here.
 
 fn main() {
 	Editor::default().run();
