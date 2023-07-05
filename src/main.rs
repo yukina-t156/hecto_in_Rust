89b1dc70de43410918015ea6bfe847d126497727
Yukina Tatsuta <ivy1200t@gmail.com>
2023-07-05 23:44:14 +0900

    Chapter4 -A line viewer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![warn(clippy::all, clippy::pedantic)]
-
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
 use editor::Editor;
-pub use terminal::Terminal; // re-exporting the `Terminal` here.
 pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal; // re-exporting the `Terminal` here.
 
 fn main() {
 	Editor::default().run();
